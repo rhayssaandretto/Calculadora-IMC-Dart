ef1ae9e8dd3e8e401aba6b96fa02d931283e8640
rhayssaandretto <rha.andretto@gmail.com>
2023-10-15 20:48:41 -0300

    mudando teste

--- test/calculadora_imc_test.dart
+++ test/calculadora_imc_test.dart
@@ -6,8 +6,4 @@ void main() {
   test('Quando calcular IMC, retornar resultado correto', () {
     expect(CalculatorIMC.calcularIMC(49, 163), equals(18.442545824080696));
   });
-
-  test('Calcular imc 0', () {
-    expect(CalculatorIMC.calcularIMC(0, 0), equals(3));
-  });
 }
